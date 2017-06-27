32d035eb029289e646c93ca6f1172874821f8956
Masahiko Tachizono <mshk@pigbo.co>
2017-06-27 15:34:12 +0900

    use Card for candidate information

--- app/src/components/Candidates/area.js
+++ app/src/components/Candidates/area.js
@@ -21,28 +21,28 @@ class Area extends Component {
   render() {
     const { className, ...props } = this.props;
     const candidates = this.props.candidates.map((candidate) => {
+      candidate.profile = candidate.age + '歳・' + candidate.sex + '・' + candidate.party + '（' + candidate.type + '）'
       return (
-          <TableRow 
-            key={candidate.name + candidate.id} 
-            selectable={false}            
-          >
-            <TableRowColumn>{candidate.name}</TableRowColumn>
-            <TableRowColumn>{candidate.age}歳・{candidate.sex}・{candidate.party}・{candidate.type}</TableRowColumn>                        
-            <TableRowColumn>
+         <Card>
+           <CardHeader
+            title={candidate.name}
+            subtitle={candidate.profile}
+           />
+           <CardText>
               <table>
                 <tbody>
                   <tr>
-                    {candidate.url && <td><a href={candidate.url} target="_blank" rel="noopener noreferrer"><FontAwesome name='home' size='2x' /></a></td> }
-                    {candidate.twitter_url && <td><a href={candidate.twitter_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='twitter' size='2x' /></a></td> }
-                    {candidate.facebook_url && <td><a href={candidate.facebook_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='facebook' size='2x' /></a></td> }
-                    {candidate.instagram_url && <td><a href={candidate.instagram_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='instagram' size='2x' /></a></td> }
-                    {candidate.youtube_url && <td><a href={candidate.youtube_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='youtube' size='2x' /></a></td> }
-                    {candidate.googleplus_url && <td><a href={candidate.googleplus_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='googleplus' size='2x' /></a></td> }
+                    {candidate.url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.url} target="_blank" rel="noopener noreferrer"><FontAwesome name='home' size='2x' /></a></td> }
+                    {candidate.twitter_url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.twitter_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='twitter' size='2x' /></a></td> }
+                    {candidate.facebook_url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.facebook_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='facebook' size='2x' /></a></td> }
+                    {candidate.instagram_url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.instagram_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='instagram' size='2x' /></a></td> }
+                    {candidate.youtube_url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.youtube_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='youtube' size='2x' /></a></td> }
+                    {candidate.googleplus_url && <td style={{ width: '2em', textAlign: 'center' }}><a href={candidate.googleplus_url} target="_blank" rel="noopener noreferrer"><FontAwesome name='googleplus' size='2x' /></a></td> }
                 </tr>
                 </tbody>
               </table>
-            </TableRowColumn>                                                                                                            
-          </TableRow>
+           </CardText>              
+          </Card>
       );
     });
 
@@ -54,18 +54,7 @@ class Area extends Component {
           actAsExpander={true}
         />
         <CardText expandable={true}>
-          <Table selectable={false} style={{ tableLayout: 'auto' }} fixedHeader={false} bodyStyle={{ overflowX: undefined, overflowY: undefined }}>
-            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-              <TableRow>
-                <TableHeaderColumn>名前</TableHeaderColumn>
-                <TableHeaderColumn>年齢・性別・党派・新 現 元</TableHeaderColumn>   
-                <TableHeaderColumn>ウェブサイト・SNS</TableHeaderColumn>   
-              </TableRow>                
-            </TableHeader>
-            <TableBody displayRowCheckbox={false}>
                 { candidates }
-            </TableBody>                        
-          </Table>
         </CardText>        
       </Card>
     )
