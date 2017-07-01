4272d451cd615c0129be244cd4e72be49a3b6179
Masahiko Tachizono <mshk@pigbo.co>
2017-07-02 03:01:19 +0900

    remove contact link from top page

--- app/src/components/Candidates/index.js
+++ app/src/components/Candidates/index.js
@@ -70,11 +70,7 @@ class Candidates extends Component {
               <li>候補者の情報は<a href="http://www.h29togisen.metro.tokyo.jp/election/list.html">東京都選挙管理委員会の立候補者一覧</a>を元にしています。</li>
               <li>上記一覧に記載の候補者ホームページを機械的に巡回し、リンクされたSNSの情報を追加しています。</li>
             </ul>
-          </p>
-          <ul>
-            <li>連絡先: <a href="https://twitter.com/mshk" target="_blank">@mshk</a></li>
-            <li>ソースコード: <a href="https://github.com/mshk/togisen2017" target="_blank">GitHub</a></li>
-          </ul>
+          </p>         
 
         </Paper>
         {candidates}
